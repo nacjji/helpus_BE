5c4310c9de76dd5e29f6d6decad3042a0155b85e
Jiwon NA <jiwonna@Jiwonui-MacBookPro.local>
2023-01-01 20:33:01 +0900

    feat : 게시물 생성 및 multer middleware 완료

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,4 +1,6 @@
+import { Request, Response, NextFunction } from 'express';
 import PostsService from '../services/posts.service';
+import { postInputPattern, postIdPattern } from '../validations/posts.validation';
 
 class PostsController {
   postsService: PostsService;
@@ -6,6 +8,39 @@ class PostsController {
   constructor() {
     this.postsService = new PostsService();
   }
+
+  // eslint-disable-next-line class-methods-use-this
+  createPost = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      //  userId = res.locals
+      const userId = 1;
+      const { title, content, updated, location1, location2 } =
+        await postInputPattern.validateAsync(req.body);
+
+      // const fileData: Express.MulterS3;
+
+      const filesArr = req.files! as Array<Express.MulterS3.File>;
+      const imageUrl = filesArr.map((file) => file.location);
+      const imageUrl1 = imageUrl[0];
+      const imageUrl2 = imageUrl[1];
+      const imageUrl3 = imageUrl[2];
+
+      const result = await this.postsService.createPost(
+        userId,
+        title,
+        content,
+        updated,
+        location1,
+        location2,
+        imageUrl1,
+        imageUrl2,
+        imageUrl3
+      );
+      return res.status(201).json({ result });
+    } catch (error) {
+      return next(error);
+    }
+  };
 }
 
 export default PostsController;
--- src/services/posts.service.ts
+++ src/services/posts.service.ts
@@ -1,11 +1,37 @@
 import PostsRepository from '../repositories/posts.repository';
+import prisma from '../config/database/prisma';
 
 class PostsService {
   postsRepository: PostsRepository;
 
   constructor() {
-    this.postsRepository = new PostsRepository();
+    this.postsRepository = new PostsRepository(prisma);
   }
+
+  createPost = async (
+    userId: number,
+    title: string,
+    content: string,
+    updated?: boolean,
+    location1?: string,
+    location2?: string,
+    imageUrl1?: string,
+    imageUrl2?: string,
+    imageUrl3?: string
+  ) => {
+    const result = await this.postsRepository.createPost(
+      userId,
+      title,
+      content,
+      updated,
+      location1,
+      location2,
+      imageUrl1,
+      imageUrl2,
+      imageUrl3
+    );
+    return result;
+  };
 }
 
 export default PostsService;
