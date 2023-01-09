6fbf0c90c566b589c14229e26f7bcd665a338e26
Jiwon NA <jiwonna@Jiwonui-MacBookPro.local>
2023-01-09 09:33:54 +0900

    chore : Chat model 설정 및 마이그레이션

--- src/socket.ts
+++ src/socket.ts
@@ -4,18 +4,26 @@ import './config/env';
 
 const Socket = (server: http.Server) => {
   const io = new Server(server, {
-    cors: {
-      origin: '*',
-    },
+    cors: { origin: '*' },
+    path: '/socket.io',
   });
 
+  const chat1 = io.of('/chat1');
   io.on('connection', (socket) => {
     console.log('New client connected');
 
     socket.on('disconnect', () => console.log('user disconnect', socket.id));
+    // 방 입장하기
+    socket.on('join-room', () => {
+      socket.join('room1');
+    });
 
-    socket.on('new_chat', (data) => {
-      io.emit('update', data);
+    const li: string[] = [];
+    // 입장한 방에 메시지 보내기
+    socket.on('room1-send', (data) => {
+      li.push(`${data} , 작성자 : ${socket.id} , 작성시간 : ${Date()}`);
+      io.to('room1').emit('broadcast', data);
+      console.log(li);
     });
   });
 };
