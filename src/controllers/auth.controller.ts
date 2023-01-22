9594d05ed878da3b5a5a590ae8ec1ad6ff0ba4fe
minalikong <minalikong@gmail.com>
2023-01-22 15:49:39 +0900

    feat: 로그인 시 쿠키 발급

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -60,11 +60,11 @@ class AuthController {
       const result = await this.authService.localLogin(email, password);
 
       // TODO: 프론트까지 배포 완료 이후 쿠키 보안 설정
-      // res.cookie('helpus_token', result.accessToken, {
-      //   sameSite: 'none',
-      //   domain: 'http://localhost:3000',
-      //   secure: false,
-      // });
+      res.cookie('helpus_token', result.token, {
+        sameSite: 'none',
+        domain: 'http://localhost:3000',
+        secure: false,
+      });
       // res.cookie('helpus_refresh', result.refreshToken, {
       //   sameSite: 'none',
       //   domain: 'http://localhost:3000',
