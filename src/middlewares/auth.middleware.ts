1d2c8839a18e440b325cd27d86531f094f9da5fa
minalikong <minalikong@gmail.com>
2023-01-23 23:43:37 +0900

    fix: Auth 미들웨어 수정

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -21,7 +21,8 @@ const requiredLogin: RequestHandler = (req, res, next) => {
 
   const payload = checkToken(helpusAccess);
   const expired = checkToken(helpusRefresh);
-  if (!payload || !expired) throw unauthorized('토큰 재발급 필요');
+  if (!payload) throw unauthorized('토큰 재발급 필요');
+  if (!expired) throw unauthorized('로그인 필요');
 
   res.locals.userId = payload.userId;
   res.locals.userName = payload.userName;
@@ -38,10 +39,8 @@ const requiredNoLogin: RequestHandler = (req, res, next) => {
   else if (!helpusAccess || !helpusRefresh) throw badRequest('비정상 토큰으로 확인됨');
   else {
     const payload = checkToken(helpusAccess);
-    const expired = checkToken(helpusRefresh);
 
-    if (!payload || !expired) throw unauthorized('토큰 재발급 필요');
-    if (payload && expired) throw unauthorized('로그인 정보 있음');
+    if (payload) throw unauthorized('로그인 정보 있음');
   }
 };
 
@@ -52,14 +51,13 @@ const passAnyway: RequestHandler = (req, res, next) => {
   else if (!helpusAccess || !helpusRefresh) throw badRequest('비정상 토큰으로 확인됨');
   else {
     const payload = checkToken(helpusAccess);
-    const expired = checkToken(helpusRefresh);
 
-    if (!payload || !expired) throw unauthorized('토큰 재발급 필요');
-
-    res.locals.userId = payload.userId;
-    res.locals.userName = payload.userName;
-    res.locals.state1 = payload.state1;
-    res.locals.state2 = payload.state2;
+    if (payload) {
+      res.locals.userId = payload.userId;
+      res.locals.userName = payload.userName;
+      res.locals.state1 = payload.state1;
+      res.locals.state2 = payload.state2;
+    }
 
     next();
   }
