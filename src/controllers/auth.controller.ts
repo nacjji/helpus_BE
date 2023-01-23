b39d9b5f01dff08dbe3ab17012b9a208b5c61fa1
minalikong <minalikong@gmail.com>
2023-01-23 18:42:22 +0900

    fix: 로그인 시 발급되는 쿠키명 변경

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -59,8 +59,8 @@ class AuthController {
       const result = await this.authService.localLogin(email, password);
 
       // TODO: 프론트까지 배포 완료 이후 쿠키 보안 설정
-      res.cookie('helpus_token', result.accessToken, { sameSite: 'none', secure: true });
-      res.cookie('helpus_refresh', result.refreshToken, { sameSite: 'none', secure: true });
+      res.cookie('helpusAccess', result.accessToken, { sameSite: 'none', secure: true });
+      res.cookie('helpusRefresh', result.refreshToken, { sameSite: 'none', secure: true });
 
       res.status(200).json({
         userId: result.userId,
