99090df5c31378c00d3736b7e109d66efa4ab4a3
Jiwon NA <jiwonna@Jiwonui-MacBookPro.local>
2023-01-31 16:14:34 +0900

    fix : 게시글 수정 오류 처리

--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -146,27 +146,26 @@ class PostsRepository {
     if (!postExist) {
       throw notFound('게시글 없음');
     }
-    return { '로그인 유저': userId, '게시글 작성자': postExist.userId || null, postExist };
-    // if (postExist.userId !== userId) {
-    //   throw unauthorized('해당 글의 작성자가 아닙니다.');
-    // }
-    // const result = await this.prisma.post.update({
-    //   where: { postId },
-    //   data: {
-    //     postId,
-    //     userId,
-    //     title,
-    //     content,
-    //     category: category || postExist.category,
-    //     appointed,
-    //     updated: 1,
-    //     isDeadLine: isDeadLine || postExist.isDeadLine,
-    //     location1,
-    //     location2,
-    //     tag,
-    //   },
-    // });
-    // return result;
+    if (postExist.userId !== userId) {
+      throw unauthorized('해당 글의 작성자가 아닙니다.');
+    }
+    const result = await this.prisma.post.update({
+      where: { postId },
+      data: {
+        postId,
+        userId,
+        title,
+        content,
+        category: category || postExist.category,
+        appointed,
+        updated: 1,
+        isDeadLine: isDeadLine || postExist.isDeadLine,
+        location1,
+        location2,
+        tag,
+      },
+    });
+    return result;
   };
 
   public deletePost = async (postId: number, userId: number) => {
