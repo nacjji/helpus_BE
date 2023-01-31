365f45ad1280924f4ee1b35a8873c3deb78e13d9
minalikong <minalikong@gmail.com>
2023-01-31 11:00:45 +0900

    fix: 액세스 토큰 30분 설정 및 maxAge 설정

--- src/services/kakao.auth.service.ts
+++ src/services/kakao.auth.service.ts
@@ -57,7 +57,7 @@ class KakaoAuthService {
         },
         JWT_SECRET_KEY,
         {
-          expiresIn: '10s',
+          expiresIn: '30m',
         }
       );
 
@@ -100,7 +100,7 @@ class KakaoAuthService {
       { userId, userName: result.userName, state1, state2 },
       JWT_SECRET_KEY,
       {
-        expiresIn: '30s',
+        expiresIn: '30m',
       }
     );
 
