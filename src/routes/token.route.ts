20c2e764c784aa87e0f5cf0a62fadf0ce5298754
minalikong <minalikong@gmail.com>
2023-02-04 14:30:17 +0900

    refactor: 사용하지 않는 import 삭제

--- src/routes/token.route.ts
+++ src/routes/token.route.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import TokenController from '../controllers/token.controller';
-import * as auth from '../middlewares/auth.middleware';
 
 const tokenRouter = Router();
 const tokenController = new TokenController();
