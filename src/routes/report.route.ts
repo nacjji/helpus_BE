89ccbd919567b06d6e3bc5150034e45ba1830679
Jiwon NA <jiwonna@Jiwonui-MacBookPro.local>
2023-02-08 18:43:37 +0900

    fix : 신고 로그인 필요 수정

--- src/routes/report.route.ts
+++ src/routes/report.route.ts
@@ -6,10 +6,6 @@ import { requiredLogin } from '../middlewares/auth.middleware';
 const reportRouter = Router();
 const reportController = new ReportController();
 
-reportRouter.post(
-  '/:postId',
-  // , requiredLogin
-  reportController.reportUser
-);
+reportRouter.post('/:postId', requiredLogin, reportController.reportUser);
 
 export default reportRouter;
